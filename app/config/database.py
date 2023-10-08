a89ea647f9797a71e3e7b2a42f864830ddb6deab
German David <germanmarinrolong@gmail.com>
2023-10-07 23:14:11 -0500

    remover valores por defecto

--- app/config/database.py
+++ app/config/database.py
@@ -8,11 +8,11 @@ def get_mysql_connection():
     load_dotenv()
 
     connection = pymysql.connect(
-        user=os.environ.get("MYSQL_USER", "root"),
-        password=os.environ.get("MYSQL_PASSWORD", "password"),
-        host=os.environ.get("MYSQL_HOST", "localhost"),
-        port=int(os.environ.get("MYSQL_PORT", "3306")),
-        database=os.environ.get("MYSQL_DBNAME", "proyecto"),
+        user=os.environ.get("MYSQL_USER"),
+        password=os.environ.get("MYSQL_PASSWORD"),
+        host=os.environ.get("MYSQL_HOST"),
+        port=int(os.environ.get("MYSQL_PORT")),
+        database=os.environ.get("MYSQL_DBNAME"),
         cursorclass=pymysql.cursors.DictCursor,
         ssl=None,
         ssl_verify_identity=None,
