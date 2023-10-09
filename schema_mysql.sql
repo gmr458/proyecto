e4e50e8813924769662e2f8c7a1499cdc091cbda
German David <germanmarinrolong@gmail.com>
2023-10-08 22:26:03 -0500

    codifo formateado

--- schema_mysql.sql
+++ schema_mysql.sql
@@ -1,36 +1,36 @@
 CREATE TABLE `usuario` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
-  `apellido` varchar(255) NOT NULL,
-  `telefono` varchar(255) UNIQUE NOT NULL,
-  `email` varchar(255) UNIQUE NOT NULL,
-  `contrasena` varchar(255) NOT NULL,
-  `numero_documento` varchar(255) UNIQUE NOT NULL,
-  `fecha_creacion` timestamp NOT NULL DEFAULT (now()),
-  `activado` boolean NOT NULL DEFAULT true
+  `id`               integer      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nombre`           varchar(255) NOT NULL,
+  `apellido`         varchar(255) NOT NULL,
+  `telefono`         varchar(255) NOT NULL UNIQUE,
+  `email`            varchar(255) NOT NULL UNIQUE,
+  `contrasena`       varchar(255) NOT NULL,
+  `numero_documento` varchar(255) NOT NULL UNIQUE,
+  `fecha_creacion`   timestamp    NOT NULL DEFAULT now(),
+  `activado`         boolean      NOT NULL DEFAULT true
 );
 
 CREATE TABLE `tarea` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(255) NOT NULL,
-  `prioridad` ENUM ('baja', 'media', 'alta') NOT NULL,
-  `usuario_id` integer NOT NULL,
-  `fecha_creacion` timestamp NOT NULL DEFAULT (now()),
-  `fecha_limite` timestamp NOT NULL,
-  `evidencia` varchar(255),
-  `estado` ENUM ('sin_iniciar', 'en_proceso', 'ejecutada') NOT NULL DEFAULT "sin_iniciar"
+  `id`             integer                        NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `titulo`         varchar(255)                   NOT NULL,
+  `prioridad`      ENUM ('baja', 'media', 'alta') NOT NULL,
+  `usuario_id`     integer                        NOT NULL,
+  `fecha_creacion` timestamp                      NOT NULL DEFAULT now(),
+  `fecha_limite`   timestamp                      NOT NULL,
+  `evidencia`      varchar(255),
+  `estado`         ENUM ('sin_iniciar', 'en_proceso', 'ejecutada') NOT NULL DEFAULT "sin_iniciar"
 );
 
 CREATE TABLE `observacion` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `tarea_id` integer NOT NULL,
-  `contenido` varchar(255) NOT NULL,
-  `fecha_creacion` timestamp NOT NULL DEFAULT (now())
+  `id`             integer      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `tarea_id`       integer      NOT NULL,
+  `contenido`      varchar(255) NOT NULL,
+  `fecha_creacion` timestamp    NOT NULL DEFAULT now()
 );
 
 CREATE TABLE `rol` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
+  `id`          integer      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nombre`      varchar(255) NOT NULL,
   `descripcion` varchar(255)
 );
 
@@ -41,22 +41,21 @@ VALUES
 
 CREATE TABLE `roles_usuario` (
   `usuario_id` integer NOT NULL,
-  `rol_id` integer NOT NULL
-
+  `rol_id`     integer NOT NULL,
   PRIMARY KEY (`usuario_id`, `rol_id`)
 );
 
 CREATE TABLE `atributo` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
+  `id`          integer      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nombre`      varchar(255) NOT NULL,
   `descripcion` varchar(255)
 );
 
 CREATE TABLE `atributos_usuario` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `usuario_id` integer NOT NULL,
-  `atributo_id` integer NOT NULL,
-  `valor` varchar(255) NOT NULL
+  `id`          integer      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `usuario_id`  integer      NOT NULL,
+  `atributo_id` integer      NOT NULL,
+  `valor`       varchar(255) NOT NULL
 );
 
 ALTER TABLE `roles_usuario` ADD FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
