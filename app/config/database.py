bd8c9c86783ad5eecb1f696be1f1aa696f4bf254
German David <germanmarinrolong@gmail.com>
2023-11-02 16:01:09 -0500

    default values and generate mysql url

--- app/config/database.py
+++ app/config/database.py
@@ -4,16 +4,26 @@ from dotenv import load_dotenv
 import pymysql
 import pymysql.cursors
 
+load_dotenv()
+
+user = os.environ.get("MYSQL_USER") or "user"
+password = os.environ.get("MYSQL_PASSWORD") or "password"
+host = os.environ.get("MYSQL_HOST") or "127.0.0.1"
+port = int(os.environ.get("MYSQL_PORT") or "3306")
+dbname = os.environ.get("MYSQL_DBNAME") or "dbname"
+
+
+def get_mysql_url():
+    return f"mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}"
+
 
 def get_mysql_connection():
-    load_dotenv()
-
     connection = pymysql.connect(
-        user=os.environ.get("MYSQL_USER"),
-        password=os.environ.get("MYSQL_PASSWORD"),
-        host=os.environ.get("MYSQL_HOST"),
-        port=int(os.environ.get("MYSQL_PORT")),
-        database=os.environ.get("MYSQL_DBNAME"),
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=dbname,
         cursorclass=pymysql.cursors.DictCursor,
         ssl=None,
         ssl_verify_identity=None,
